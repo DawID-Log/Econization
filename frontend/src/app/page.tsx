787ea4b86c09b531bbfda04463e3a5ce2e667654
DawID-Log <dawid.logoluso@gmail.com>
2024-05-14 00:40:03 +0200

    [MAIN] Created app project

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import HomePage from "./pages/HomePage/homePage";
+
+export default function Home() {
+  return (
+    <main>
+      <HomePage />
+    </main>
+  );
+}
---
+++ frontend/src/app/pages/HomePage/homePage.tsx
@@ -0,0 +1,9 @@
+
+export default function HomePage() {
+    return (
+      <main>
+        
+      </main>
+    );
+  }
+  
